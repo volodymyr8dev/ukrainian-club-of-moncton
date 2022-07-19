a5bd104e16f88bd58d2fa4cd3516c3fa803b5cee
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-07-19 10:27:45 -0300

    fix: Add cursor pointer to donate

--- components/utils/DonateButton.jsx
+++ components/utils/DonateButton.jsx
@@ -8,7 +8,7 @@ export const DonateButton = () => {
     <>
     <Link href='./'>
       <div
-        className='font-proximaNova500 text-xl bg-blue-500 py-[14px]
+        className='cursor-pointer font-proximaNova500 text-xl bg-blue-500 py-[14px]
         px-[32px] rounded-[50px] uppercase flex gap-4'>
         <span className='text-gray-100'>
           Donate now
