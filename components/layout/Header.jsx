bc6d7bf57ba1da884820fb0424017b1014ebee2b
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-07-20 14:53:30 -0300

    feat: Mobile menu toggler

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,33 +1,56 @@
+import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 
 import languageSwitch from './../../assets/images/language-switch.svg'
+import menuIcon from './../../assets/images/menu-icon.svg'
 import { DonateButton } from '../utils/DonateButton'
 
 export const Header = () => {
+  const [isActive, setIsActive] = useState(false)
+
+  const toggleMenu = () => {
+    setIsActive(!isActive)
+  }
+
   return (
     <header
       className='flex justify-center h-20 shadow-[0px_4px_4px_rgba(0,0,0,0.06)]'
     >
       <nav
-        className='flex justify-between items-center max-w-[1216px] w-full'
+        className='flex justify-between items-center max-w-[1216px] w-full px-7 xl:px-0'
       >
-        <div>
-          <ul className='flex align-center gap-16'>
-            <li className='font-proximaNova500 text-xl text-blue-500'>
+        <div className={`md:flex ${ isActive ? 'flex bg-gray-100 absolute w-full h-full top-0 -mx-7 justify-center z-10' : 'hidden' }`}>
+          <ul className={`flex align-center gap-16 flex-col md:flex-row translate-y-[200px] md:translate-y-0`}>
+            <li className='font-proximaNova500 text-xl text-center text-blue-500'>
               <Link href='/'>Home</Link>
             </li>
-            <li className='font-proximaNova500 text-xl'>
+            <li className='font-proximaNova500 text-xl text-center'>
               <Link href='/'>Info</Link>
             </li>
-            <li className='font-proximaNova500 text-xl'>
+            <li className='font-proximaNova500 text-xl text-center'>
               <Link href='/'>Events</Link>
             </li>
-            <li className='font-proximaNova500 text-xl'>
+            <li className='font-proximaNova500 text-xl text-center'>
               <Link href='/'>About</Link>
             </li>
           </ul>
         </div>
+
+        <div className='flex md:hidden z-20'>
+          <button
+            className=''
+            onClick={ toggleMenu }
+            aria-label='toggle menu'
+          >
+            <Image
+              src={ menuIcon }
+              width={ 16 }
+              height={ 16 }
+            />
+          </button>
+        </div>
+
         <div>
           <ul className='flex align-center gap-8'>
             <li className='flex align-center'>
