ee8d2ea28e4e3e8edb85fc8bf12af3529b31caad
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-07-20 11:47:06 -0300

    feat: BottomCTA component

--- components/sections/home/BottomCTA.jsx
+++ components/sections/home/BottomCTA.jsx
@@ -2,11 +2,21 @@ import { DonateButton } from './../../utils/DonateButton'
 
 export const BottomCTA = () => {
   return (
-    <section className='flex justify-center mb-24'>
+    <section className='flex justify-center mt-24 mb-24 '>
     <div className='flex justify-center flex-col max-w-[1216px] w-full'>
       <div className='flex flex-col justify-center px-9 overflow-visible'>
-        <div className='flex justify-center -translate-y-12 z-50'>
+        <div className='flex justify-center gap-4'>
           <DonateButton />
+          <a href='#'>
+            <div
+              className='cursor-pointer font-proximaNova400 border-solid
+              border-blue-500 border-2 text-xl py-[12px]
+              px-[32px] rounded-[50px] uppercase'>
+              <span className='text-blue-500'>
+                Do you want to volunteer with us?
+              </span>
+            </div>
+          </a>
         </div>
       </div>
     </div>
--- components/utils/DonateButton.jsx
+++ components/utils/DonateButton.jsx
@@ -7,7 +7,7 @@ export const DonateButton = () => {
     <>
     <a href='#'>
       <div
-        className='cursor-pointer font-proximaNova500 text-xl bg-blue-500 py-[14px]
+        className='cursor-pointer font-proximaNova400 text-xl bg-blue-500 py-[14px]
         px-[32px] rounded-[50px] uppercase flex gap-4'>
         <span className='text-gray-100'>
           Donate now
