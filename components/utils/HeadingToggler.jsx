6d1f0f939e0854109b27234caee52e093d842f23
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-07-21 10:39:23 -0300

    Club questions semi-responsive

--- components/utils/HeadingToggler.jsx
+++ components/utils/HeadingToggler.jsx
@@ -5,13 +5,14 @@ import Image from 'next/image'
 export const HeadingToggler = (props) => {
   return (
     <>
-      <div className='flex justify-between items-center pb-16'>
+      <div className='flex justify-between items-center pb-12 md:pb-16'>
         <div>
-          <h2 className='font-proximaNova400 text-[40px] uppercase'>
+          <h2 className='font-proximaNova400 text-[32px] md:text-[40px] uppercase
+          tracking-wider'>
             { props.heading }
           </h2>
         </div>
-        <div className='flex gap-8'>
+        <div className='hidden md:flex gap-8'>
           <Image
             src={ prevArrow }
             width={ 64 }
