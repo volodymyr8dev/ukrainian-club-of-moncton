731eeb1ee2ef473729b5ced4488f231ece04bc5b
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-07-21 14:17:24 -0300

    feat: Footer responsive

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -5,12 +5,13 @@ import UCMLogo from './../../assets/images/ucm-logo.svg'
 export const Footer = () => {
   return (
     <footer                                      
-      className='flex justify-center h-[120px] px-6 xl:px-0
-      shadow-[0px_-6px_8px_-4px_rgba(0,0,0,0.06)]'
+      className='flex justify-center h-full md:h-[120px] px-6 xl:px-0
+      shadow-[0px_-6px_8px_-4px_rgba(0,0,0,0.06)] pt-8 pb-4 md:pt-0 md:pb-0'
     >
-      <div className='flex justify-between items-center max-w-[1216px] w-full'>
+      <div className='flex justify-between items-center max-w-[1216px] w-full
+      flex-col md:flex-row'>
         <div>
-          <ul className='flex items-center gap-4'>
+          <ul className='flex items-center gap-0 md:gap-4 flex-col md:flex-row'>
             <li>
               <Link href='/'>
                 <Image
@@ -18,22 +19,22 @@ export const Footer = () => {
                 />
               </Link>
             </li>
-            <li className='flex flex-col'>
-              <span>
+            <li className='flex flex-col text-center md:text-left'>
+              <span className='text-xs md:text-sm'>
                 Contact us:
                 {' '}
                 <Link href='/'>
                   +222-2222-2222
                 </Link>
               </span>
-              <span>
+              <span className='text-xs md:text-sm'>
                 Email:
                 {' '}
                 <Link href='/'>
                   info@ukrclubmoncton.ca
                 </Link>
               </span>
-              <span>
+              <span className='text-xs md:text-sm'>
                 Address:
                 {' '}
                 <Link href='/'>
@@ -45,8 +46,10 @@ export const Footer = () => {
         </div>
         <div>
           <ul>
-            <li>
-              <span>By <Link href='https://binaryfuture.io'>BinaryFuture.io</Link></span>
+            <li className='pt-4 md:pt-0'>
+              <span className='text-xs md:text-sm'>
+                By <Link href='https://binaryfuture.io'>BinaryFuture.io</Link>
+              </span>
             </li>
           </ul>
         </div>
