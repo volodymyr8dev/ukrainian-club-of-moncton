9beae3259549272a8b8230aa465e21660df17f00
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-07-21 13:03:53 -0300

    fix: (Ongoing) Gallery responsive mobile

--- components/utils/Gallery.jsx
+++ components/utils/Gallery.jsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import 'swiper/css'
 import 'swiper/css/pagination'
+import 'swiper/css/grid'
 import 'swiper/css/autoplay'
 
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { Pagination, Autoplay } from 'swiper'
+import { Pagination, Grid, Autoplay } from 'swiper'
 import galleryImage1 from './../../assets/images/sample-photo-1.svg'
 import galleryImage2 from './../../assets/images/sample-photo-2.svg'
 
@@ -12,16 +13,29 @@ export const Gallery = () => {
   return (
     <>
       <Swiper
+        breakpoints={{
+          100: {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+            slidesPerColumn: 2,
+            slidesPerRow: 2,
+            grid: {
+              rows: 2,
+            }
+          },
+          768: {
+            slidesPerView: 3,
+          },
+        }}
+        spaceBetween={ 30 }
         autoplay={{
           delay: 2500,
           disableOnInteraction: false,
         }}
-        slidesPerView={3}
-        spaceBetween={30}
         pagination={{
           clickable: true,
         }}
-        modules={[ Pagination, Autoplay ]}
+        modules={[ Pagination, Grid, Autoplay ]}
         className='home-gallery-swiper'
       >
         <SwiperSlide>
