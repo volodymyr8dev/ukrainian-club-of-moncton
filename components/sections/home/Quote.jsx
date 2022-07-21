2fb24433eb46f995d862d9af56e6558bb96deaef
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-07-21 13:10:21 -0300

    feat: Quote mobile responsive

--- components/sections/home/Quote.jsx
+++ components/sections/home/Quote.jsx
@@ -4,10 +4,10 @@ import quoteImage from './../../../assets/images/quote.svg'
 
 export const Quote = () => {
   return (
-    <section className='flex justify-center mb-12 mt-36 px-6 xl:px-0'>
+    <section className='flex justify-center mb-12 mt-0 md:mt-36 px-6 xl:px-0'>
     <div className='flex justify-center flex-col max-w-[1216px] w-full'>
       <div className='flex flex-col justify-center rounded-3xl overflow-visible
-      shadow-[0px_2px_32px_rgba(0,32,73,0.13)] px-9'>
+      shadow-[0px_2px_32px_rgba(0,32,73,0.13)] px-4 md:px-9'>
         <div className='flex justify-center -translate-y-12 z-50'>
           <Image
             src={ quoteImage }
@@ -15,10 +15,13 @@ export const Quote = () => {
         </div>
 
         <div className='-mt-5 pb-10'>
-          <p className='text-center font-proximaNova300 text-lg'>
+          <p className='text-left md:text-center font-proximaNova300 text-base md:text-lg
+          leading-4'>
             <span className='max-w-2xl w-full inline-block'>
               We are an independent, non-profit, self-sufficient club founded and 
-              chartered in 1975. Our membership consists of people of all ages and 
+              chartered in 1975.
+              <br/>
+              Our membership consists of people of all ages and 
               walks of life. 
             </span>
             <br />
