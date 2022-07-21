4cad76f5e361ced5a185542de3f704bdbec279a9
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-07-21 10:09:43 -0300

    fix: Resolve a few tablets breakpoint issues

--- components/sections/home/HomeFront.jsx
+++ components/sections/home/HomeFront.jsx
@@ -12,8 +12,8 @@ export const HomeFront = () => {
     >
       <div className='flex justify-between gap-6 flex-col tablets:flex-row
       w-full tablets:w-auto'>
-        <div className='lg:max-w-[340px] max-w-[280px] 2xl:max-w-[540px] flex
-        flex-col gap-6 md:gap-8 lg:-mt-8 mt-12 tablets:-mt-8'>
+        <div className='lg:max-w-[340px] max-w-full 2xl:max-w-[540px] flex
+        flex-col gap-6 md:gap-8 lg:-mt-8 mt-12 tablets:mt-16'>
           <h2 className='font-proximaNova300 text-[34px] md:text-[50px]
           leading-[40px] md:leading-[65px] first-letter:tracking-wide'>
             ДОБРОГО ВЕЧОРА
@@ -49,7 +49,9 @@ export const HomeFront = () => {
           </div>
         </div>
 
-        <div className='2xl:translate-y-0 md:translate-y-[90px] translate-y-0'>
+        <div className='2xl:translate-y-0 md:translate-y-[90px] translate-y-0
+        flex justify-center
+        '>
           <Image
             src={ birdImage }
             width={ 454 }
@@ -57,8 +59,8 @@ export const HomeFront = () => {
           />
         </div>
 
-        <div className='lg:max-w-[340px] max-w-[280px] 2xl:max-w-[540px] w-full
-        hidden tablets:flex flex-col gap-8 mt-0 md:mt-[14.4rem]'>
+        <div className='lg:max-w-[340px] max-w-full 2xl:max-w-[540px] w-full
+        hidden md:flex flex-col gap-8 mt-0 md:mt-[14.4rem]'>
           <div className='flex justify-start items-center gap-6 md:gap-8'>
             <FilledButton
               text='Що далі?'
@@ -97,7 +99,7 @@ export const HomeFront = () => {
           
         </div>
 
-        <div className='max-w-full flex tablets:hidden flex-col gap-6 mt-2
+        <div className='max-w-full flex md:hidden flex-col gap-6 mt-2
         tablets:mt-20'>
           <h2 className='font-proximaNova300 text-[23px] uppercase
           leading-[26px] tracking-wider'>
