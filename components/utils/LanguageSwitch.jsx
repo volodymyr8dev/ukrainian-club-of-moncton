b6075598a12887e5cc866e5eed5b5f20e1fcae33
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-07-25 15:21:02 -0300

    debug pinpoint

--- components/utils/LanguageSwitch.jsx
+++ components/utils/LanguageSwitch.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
+import { Link } from 'next/link'
 
 import MenuItem from '@mui/material/MenuItem'
 import FormControl from '@mui/material/FormControl'
 import Select from '@mui/material/Select'
 
 export const LanguageSwitch = () => {
+
   const [language, setLanguage] = useState('')
 
   const handleChange = (event) => {
@@ -18,7 +20,6 @@ export const LanguageSwitch = () => {
           <Select
             sx={{ borderColor: 'white', border: 0, }}
             value={ language }
-            onChange={handleChange}
             displayEmpty
             disableInjectingGlobalStyles
             inputProps={{ 'aria-label': 'Without label' }}
@@ -27,18 +28,23 @@ export const LanguageSwitch = () => {
               <span className='opacity-40 font-proximaNova500 text-base
               md:text-xl pr-2 flex items-center' >ENG</span>
             </MenuItem>
-            <MenuItem value='ENG'>
-              <span className='font-proximaNova500 text-base md:text-xl pr-2 flex
-              items-center'>
-                ENG
-              </span>
-            </MenuItem>
-            <MenuItem value='UKR'>
-              <span className='font-proximaNova500 text-base md:text-xl pr-2 flex
-              items-center'>
-                UKR
-              </span>
-            </MenuItem>
+              <MenuItem value='ENG'>
+                <a href='/'>
+                  <span className='font-proximaNova500 text-base md:text-xl pr-2 flex
+                  items-center'>
+                    ENG
+                  </span>
+                </a>
+              </MenuItem>
+            
+              <MenuItem value='UKR'>
+                <Link href='/uk'>
+                  <span className='font-proximaNova500 text-base md:text-xl pr-2 flex
+                  items-center'>
+                    UKR
+                  </span>
+                </Link>
+              </MenuItem>
           </Select>
         </FormControl>
       </div>
