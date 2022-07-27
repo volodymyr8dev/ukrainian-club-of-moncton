c94c47816a2eafeb3d35ab5bab638e2287172125
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-07-27 13:55:59 -0300

    fix: Correct image asset and styles for mobile

--- components/sections/about/AboutFront.jsx
+++ components/sections/about/AboutFront.jsx
@@ -1,9 +1,9 @@
 import Image from 'next/image'
 import useTranslation from 'next-translate/useTranslation'
 
-import Farm1 from './../../../assets/images/farm-1.png'
-import Farm2 from './../../../assets/images/farm-2.png'
-import Farm3 from './../../../assets/images/farm-3.png'
+import Farm1 from './../../../assets/images/farm/farm-01.svg'
+import Farm2 from './../../../assets/images/farm/farm-02.svg'
+import Farm3 from './../../../assets/images/farm/farm-03.svg'
 
 export const AboutFront = () => {
   let { t } = useTranslation()
@@ -14,20 +14,21 @@ export const AboutFront = () => {
       className='flex justify-center items-center mb-[60px]'
     >
       <div className='flex justify-between gap-6 flex-col'>
-        <div className='w-screen flex justify-between gap-1 tablets:gap-[18px]'>
-          <div className='w-full'>
+        <div className='about-front-farm w-screen flex justify-between gap-1
+        tablets:gap-[18px]'>
+          <div className='w-full overflow-hidden'>
             <Image
               src={ Farm1 }
               layout='responsive'
             />
           </div>
-          <div className='w-full'>
+          <div className='w-full overflow-hidden'>
             <Image
               src={ Farm2 }
               layout='responsive'
             />
           </div>
-          <div className='w-full'>
+          <div className='w-full overflow-hidden'>
             <Image
               src={ Farm3 }
               layout='responsive'
