fe8172b2211807a42f877c3b40f897c80803bf6d
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-07-27 15:36:22 -0300

    feat: Add PayPal button url with hosted button id as a environment variable

--- components/utils/DonateButton.jsx
+++ components/utils/DonateButton.jsx
@@ -3,13 +3,18 @@ import Image from 'next/image'
 import yellowHeart from './../../assets/images/yellow-heart.svg'
 
 export const DonateButton = () => {
+  const hostedButtonId = process.env.NEXT_PUBLIC_HOSTED_BUTTON_ID
+
   return (
     <>
-    <a href='#'>
+    <a
+      href={`https://www.sandbox.paypal.com/donate/?hosted_button_id=${hostedButtonId}`}
+      target='_blank'
+    >
       <div
-        className='cursor-pointer font-proximaNova400 text-base md:text-xl bg-blue-500
-        py-3 md:py-[14px] px-3 smaller-phones:px-6 small-phones:px-[32px] rounded-[50px]
-        uppercase flex gap-4'>
+        className='cursor-pointer font-proximaNova400 text-base md:text-xl
+        bg-blue-500 py-3 md:py-[14px] px-3 smaller-phones:px-6
+        small-phones:px-[32px] rounded-[50px] uppercase flex gap-4'>
         <span className='text-gray-100'>
           Donate now
         </span>
