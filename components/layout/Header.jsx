f34516defc0f6b788709188e496a9cd37b1bc29d
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-07-27 15:05:33 -0300

    feat: Implement active header link based on router-page

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 
 import menuIcon from './../../assets/images/menu-icon.svg'
 import { DonateButton } from '../utils/DonateButton'
 import { LanguageSwitch } from '../utils/LanguageSwitch'
 
 export const Header = () => {
+  const router = useRouter()
+
   const [isActive, setIsActive] = useState(false)
 
   const toggleMenu = () => {
@@ -23,18 +26,25 @@ export const Header = () => {
         xl:px-0'
       >
         <div className={`md:flex ${ isActive ? 'flex bg-gray-100 absolute w-full h-full top-0 -mx-7 justify-center z-10' : 'hidden' }`}>
-          <ul className={`flex items-center gap-16 flex-col md:flex-row translate-y-[200px] md:translate-y-0 bg-gray-100 h-screen md:h-full w-screen md:w-full pt-6 md:pt-0 -mt-[130px] md:mt-0`}>
-            <li className='py-6 font-proximaNova500 text-xl text-center
-            text-blue-500 border-b-2 border-blue-500'>
-              <Link href='/'>Home</Link>
+          <ul className='flex items-center gap-16 flex-col md:flex-row
+          translate-y-[200px] md:translate-y-0 bg-gray-100 h-screen md:h-full
+          w-screen md:w-full pt-6 md:pt-0 -mt-[130px] md:mt-0'>
+            <li className={`py-6 font-proximaNova500 text-xl text-center border-b-2
+            ${ router.pathname == '/' ? 'text-blue-500 border-blue-500' : '' }`}>
+              <Link href='/'>
+                <a>Home</a>
+              </Link>
             </li>
-            <li className='py-6 font-proximaNova500 text-xl text-center'>
+            <li className={`py-6 font-proximaNova500 text-xl text-center border-b-2
+            ${ router.pathname == '/info' ? 'text-blue-500 border-blue-500' : '' }`}>
               <Link href='/info'>Info</Link>
             </li>
-            <li className='py-6 font-proximaNova500 text-xl text-center'>
+            <li className={`py-6 font-proximaNova500 text-xl text-center border-b-2
+            ${ router.pathname == '/events' ? 'text-blue-500 border-blue-500' : '' }`}>
               <Link href='/events'>Events</Link>
             </li>
-            <li className='py-6 font-proximaNova500 text-xl text-center'>
+            <li className={`py-6 font-proximaNova500 text-xl text-center border-b-2
+            ${ router.pathname == '/about' ? 'text-blue-500 border-blue-500' : '' }`}>
               <Link href='/about'>About</Link>
             </li>
           </ul>
