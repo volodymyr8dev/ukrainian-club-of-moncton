a62f89a0dd281bc47d47256ab6e8646104812336
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-08-03 10:20:22 -0300

    feat: Use actual data on upcoming events and move to main component

--- components/sections/home/UpcomingEvents.jsx
+++ components/sections/home/UpcomingEvents.jsx
@@ -1,3 +1,8 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import moment from 'moment'
+
+import { gql, useQuery } from '@apollo/client'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Pagination, Navigation } from 'swiper'
 
@@ -7,10 +12,38 @@ import 'swiper/css/navigation'
 import 'swiper/css/grid'
 import 'swiper/css/autoplay'
 
+import locationImage from './../../../assets/images/card-location.svg'
+
 import { HeadingToggler } from '../../utils/HeadingToggler'
-import { EventCard } from '../../utils/EventCard'
+
+const GET_MOST_RECENT_POSTS_QUERY = gql`
+  query getRecentPosts {
+    posts(
+      orderBy: createdAt_ASC,
+      last: 6
+    ) {
+    title
+      tags {
+        name
+        slug
+      }
+      excerpt
+      featuredImage {
+        url
+      }
+      address
+      slug
+      createdAt
+    }
+  }
+`
 
 export const UpcomingEvents = () => {
+  const { loading, error, data } = useQuery(GET_MOST_RECENT_POSTS_QUERY)
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
   return (
     <>
       <section className='flex justify-center mb-16 md:mb-28 pl-6 md:px-6 xl:px-0'>
@@ -48,18 +81,71 @@ export const UpcomingEvents = () => {
               }}
               modules={[ Navigation, Pagination ]}
             >
+            { data.posts.map(post => (
               <SwiperSlide className='py-6'>
-                <EventCard />
+                <div
+                  key={ post.title }
+                  className='shadow-[0px_2px_32px_rgba(0,32,73,0.13)] max-w-full
+                  w-full max-h-full h-full overflow-hidden
+                  rounded-3xl'
+                >
+                  <div className='w-full'>
+                    <img
+                      className='w-full min-h-[200px] md:min-h-[256px] object-cover'
+                      src={ post.featuredImage.url }
+                      alt={ post.title }
+                      title={ post.title }
+                      loading='lazy'
+                    />
+                  </div>
+                  <div className='px-4 lg:px-6 pt-6'>
+                    <span className='font-proximaNova200 bg-yellow-100 text-yellow-900
+                    px-6 py-2 rounded-[20px]'>
+                      { post.tags[0].name }
+                    </span>
+
+                    <div className='pt-8'>
+                      <h6 className='font-proximaNova500 uppercase text-xl md:text-2xl'>
+                        { post.title }
+                      </h6>
+                      <p className='pr-4 md:pr-0 font-proximaNova200 text-base md:text-lg
+                      pt-2 leading-[18px]'>
+                        { post.excerpt }
+                      </p>
+
+                      <div className='flex gap-[18px] items-center pt-6'>
+                        <Image
+                          src={ locationImage }
+                          width={ 18.8 }
+                          height={ 24 }
+                        />
+
+                        <span className='font-proximaNova200 text-gray-500 text-base
+                        md:text-lg leading-[18px]'>
+                          { post.address }
+                        </span>
+                      </div>
+
+                      <div className='w-full md:w-auto flex justify-between items-center 
+                      pt-10 pb-6'>
+                        <span className='font-proximaNova200 text-gray-500 text-base
+                        md:text-lg leading-[18px]'>
+                          { moment(post.createdAt).format('MMM DD, YYYY') }
+                        </span>
+
+                        <Link href='/placeholder'>
+                          <span className='bg-yellow-500 text-gray-100 py-3 px-8 lg:px-10
+                          rounded-[64px] font-proximaNova400 text-base md:text-lg text-center
+                          cursor-pointer'>
+                            Read more
+                          </span>
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </SwiperSlide>
-              <SwiperSlide className='py-6'>
-                <EventCard />
-              </SwiperSlide>
-              <SwiperSlide className='py-6'>
-                <EventCard />
-              </SwiperSlide>
-              <SwiperSlide className='py-6'>
-                <EventCard />
-              </SwiperSlide>
+            ))}
             </Swiper>
           </div>
         </div>
