c02b352d09c69668310e0d05719f6d33f2eb99b6
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-08-03 11:39:47 -0300

    feat: Add filtering depending on tab value on info page

--- components/sections/info/PostsDisplay.jsx
+++ components/sections/info/PostsDisplay.jsx
@@ -12,10 +12,37 @@ import 'swiper/css/pagination'
 import 'swiper/css/grid'
 import 'swiper/css/autoplay'
 
-const GET_MOST_RECENT_POSTS_QUERY = gql`
-  query getRecentPosts {
+const GET_MOST_RECENT_IN_CANADA_POSTS_QUERY = gql`
+  query getRecentInCanadaPosts {
     posts(
-      where: {category: {slug: "event"}}
+      where: {category: {slug: "in-canada"}}
+      orderBy: createdAt_DESC,
+      last: 6
+    ) {
+    title
+      tags {
+        name
+        slug
+      }
+      category {
+        name
+        slug
+      }
+      excerpt
+      featuredImage {
+        url
+      }
+      address
+      slug
+      createdAt
+    }
+  }
+`
+
+const GET_MOST_RECENT_GOING_TO_CANADA_POSTS_QUERY = gql`
+  query getRecentGoingToCanadaPosts {
+    posts(
+      where: {category: {slug: "going-to-canada"}}
       orderBy: createdAt_DESC,
       last: 6
     ) {
@@ -47,10 +74,24 @@ export const PostsDisplay = () => {
   const handleTabSwitch = () => {
     setActiveTab(!activeTab)
   }
-  const { loading, error, data } = useQuery(GET_MOST_RECENT_POSTS_QUERY)
 
-  if (loading) return <p>Loading...</p>
-  if (error) return <p>Error</p>
+  const {
+    loading: loadingIn,
+    error: errorIn,
+    data: dataIn
+  } = useQuery(GET_MOST_RECENT_IN_CANADA_POSTS_QUERY)
+
+  const {
+    loading: loadingTo,
+    error: errorTo,
+    data: dataTo
+  } = useQuery(GET_MOST_RECENT_GOING_TO_CANADA_POSTS_QUERY)
+
+  if (loadingIn) return <p>Loading...</p>
+  if (errorIn) return <p>Error</p>
+
+  if (loadingTo) return <p>Loading...</p>
+  if (errorTo) return <p>Error</p>
 
   const pagination = {
     clickable: true,
@@ -120,56 +161,109 @@ export const PostsDisplay = () => {
               pagination={ pagination }
               modules={[ Pagination ]}
             >
-              { data.posts.map(post => (
-                <SwiperSlide className='py-6'>
-                  <div className='shadow-[0px_2px_22px_rgba(0,32,73,0.13)] max-w-full
-                  w-full max-h-full h-full overflow-hidden bg-gray-100
-                  rounded-3xl'>
-                    <div className='w-full'>
-                      <img
-                        className='w-full min-h-[200px] md:min-h-[256px]
-                        object-cover'
-                        src={ post.featuredImage.url }
-                        alt={ post.title }
-                        title={ post.title }
-                        loading='lazy'
-                      />
-                    </div>
-                    <div className='px-4 lg:px-6 pt-6'>
-                      <span className='font-proximaNova200 bg-yellow-100 text-yellow-900
-                      px-6 py-2 rounded-[20px]'>
-                        { post.tags[0].name }
-                      </span>
-
-                      <div className='pt-8'>
-                        <h6 className='font-proximaNova500 uppercase text-xl md:text-2xl'>
-                          { post.title }
-                        </h6>
-                        <p className='pr-4 md:pr-0 font-proximaNova200 text-base md:text-lg
-                        pt-2 leading-[18px]'>
-                          { post.excerpt }
-                        </p>
-
-                        <div className='w-full md:w-auto flex justify-between items-center 
-                        pt-10 pb-6'>
-                          <span className='font-proximaNova200 text-gray-500 text-base
-                          md:text-lg leading-[18px]'>
-                            { moment(post.createdAt).format('MMM DD, YYYY') }
-                          </span>
-
-                          <Link href='/placeholder'>
-                            <span className='bg-yellow-500 text-gray-100 py-3 px-8 lg:px-10
-                            rounded-[64px] font-proximaNova400 text-base md:text-lg text-center
-                            cursor-pointer'>
-                              Read more
+              {
+                activeTab
+                ? dataIn.posts.map(post => (
+                  <SwiperSlide className='py-6'>
+                    <div className='shadow-[0px_2px_22px_rgba(0,32,73,0.13)] max-w-full
+                    w-full max-h-full h-full overflow-hidden bg-gray-100
+                    rounded-3xl'>
+                      <div className='w-full'>
+                        <img
+                          className='w-full min-h-[200px] md:min-h-[256px]
+                          object-cover'
+                          src={ post.featuredImage.url }
+                          alt={ post.title }
+                          title={ post.title }
+                          loading='lazy'
+                        />
+                      </div>
+                      <div className='px-4 lg:px-6 pt-6'>
+                        <span className='font-proximaNova200 bg-yellow-100 text-yellow-900
+                        px-6 py-2 rounded-[20px]'>
+                          { post.tags[0].name }
+                        </span>
+  
+                        <div className='pt-8'>
+                          <h6 className='font-proximaNova500 uppercase text-xl md:text-2xl'>
+                            { post.title }
+                          </h6>
+                          <p className='pr-4 md:pr-0 font-proximaNova200 text-base md:text-lg
+                          pt-2 leading-[18px]'>
+                            { post.excerpt }
+                          </p>
+  
+                          <div className='w-full md:w-auto flex justify-between items-center 
+                          pt-10 pb-6'>
+                            <span className='font-proximaNova200 text-gray-500 text-base
+                            md:text-lg leading-[18px]'>
+                              { moment(post.createdAt).format('MMM DD, YYYY') }
                             </span>
-                          </Link>
+  
+                            <Link href='/placeholder'>
+                              <span className='bg-yellow-500 text-gray-100 py-3 px-8 lg:px-10
+                              rounded-[64px] font-proximaNova400 text-base md:text-lg text-center
+                              cursor-pointer'>
+                                Read more
+                              </span>
+                            </Link>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                </SwiperSlide>
-              ))}
+                  </SwiperSlide>
+                ))
+                : dataTo.posts.map(post => (
+                  <SwiperSlide className='py-6'>
+                    <div className='shadow-[0px_2px_22px_rgba(0,32,73,0.13)] max-w-full
+                    w-full max-h-full h-full overflow-hidden bg-gray-100
+                    rounded-3xl'>
+                      <div className='w-full'>
+                        <img
+                          className='w-full min-h-[200px] md:min-h-[256px]
+                          object-cover'
+                          src={ post.featuredImage.url }
+                          alt={ post.title }
+                          title={ post.title }
+                          loading='lazy'
+                        />
+                      </div>
+                      <div className='px-4 lg:px-6 pt-6'>
+                        <span className='font-proximaNova200 bg-yellow-100 text-yellow-900
+                        px-6 py-2 rounded-[20px]'>
+                          { post.tags[0].name }
+                        </span>
+  
+                        <div className='pt-8'>
+                          <h6 className='font-proximaNova500 uppercase text-xl md:text-2xl'>
+                            { post.title }
+                          </h6>
+                          <p className='pr-4 md:pr-0 font-proximaNova200 text-base md:text-lg
+                          pt-2 leading-[18px]'>
+                            { post.excerpt }
+                          </p>
+  
+                          <div className='w-full md:w-auto flex justify-between items-center 
+                          pt-10 pb-6'>
+                            <span className='font-proximaNova200 text-gray-500 text-base
+                            md:text-lg leading-[18px]'>
+                              { moment(post.createdAt).format('MMM DD, YYYY') }
+                            </span>
+  
+                            <Link href='/placeholder'>
+                              <span className='bg-yellow-500 text-gray-100 py-3 px-8 lg:px-10
+                              rounded-[64px] font-proximaNova400 text-base md:text-lg text-center
+                              cursor-pointer'>
+                                Read more
+                              </span>
+                            </Link>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                ))
+              }
             </Swiper>
           </div>
         </div>
