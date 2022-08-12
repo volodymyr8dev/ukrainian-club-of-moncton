33e80f3bd1b835b78ff52363eb0ea19d52366592
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-08-12 17:10:40 -0300

    fix: Change events href

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -107,7 +107,7 @@ export const Header = () => {
             text-base md:text-xl text-center md:border-b-2 w-full md:w-auto px-7
             md:px-0 capitalize
             ${ pathname === '/events' ? 'md:text-blue-500 md:border-blue-500 bg-blue-500 md:bg-gray-100 rounded-lg md:rounded-none px-4 md:px-0' : '' }`}>
-              <Link href='/events' locale={locale}>
+              <Link href='/events/1' locale={locale}>
                 <div className='flex md:block items-center justify-start gap-4
                 md-gap-0'>
                   <div className='flex md:hidden items-center'>
