21d3e379c2e4e4e7ca601cc5df3cb9e17d502ab6
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-08-15 20:25:50 -0300

    fix: Images for our help page

--- components/sections/moneyhelped/MoneyHelpedFront.jsx
+++ components/sections/moneyhelped/MoneyHelpedFront.jsx
@@ -1,13 +1,13 @@
 import Image from 'next/image'
 import useTranslation from 'next-translate/useTranslation'
 
-import Img1Desktop from './../../../assets/images/info/img-d-1.svg'
-import Img2Desktop from './../../../assets/images/info/img-d-2.svg'
-import Img3Desktop from './../../../assets/images/info/img-d-3.svg'
+import Img1Desktop from './../../../assets/images/our-help/img-d-1.svg'
+import Img2Desktop from './../../../assets/images/our-help/img-d-2.svg'
+import Img3Desktop from './../../../assets/images/our-help/img-d-3.svg'
 
-import Img1Mobile from './../../../assets/images/info/img-m-1.svg'
-import Img2Mobile from './../../../assets/images/info/img-m-2.svg'
-import Img3Mobile from './../../../assets/images/info/img-m-3.svg'
+import Img1Mobile from './../../../assets/images/our-help/img-m-1.svg'
+import Img2Mobile from './../../../assets/images/our-help/img-m-2.svg'
+import Img3Mobile from './../../../assets/images/our-help/img-m-3.svg'
 
 export const MoneyHelpedFront = () => {
   let { t } = useTranslation()
