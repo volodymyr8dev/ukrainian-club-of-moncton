921375487ef4ea2656d3e1e69d08b77cc404780b
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-08-15 13:32:30 -0300

    fix: Info, Events and Our-help URLS with next.config

--- next.config.js
+++ next.config.js
@@ -6,5 +6,24 @@ module.exports = {
   images: {
     domains: ['media.graphassets.com'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/info',
+        destination: '/info/in-canada/1',
+        permanent: true,
+      },
+      {
+        source: '/events',
+        destination: '/events/1',
+        permanent: true,
+      },
+      {
+        source: '/our-help',
+        destination: '/our-help/1',
+        permanent: true,
+      },
+    ]
+  },
   ...nextTranslate()
 }
