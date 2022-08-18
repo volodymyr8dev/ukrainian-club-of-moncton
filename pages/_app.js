b47f6dd0355c8cfd01b016086d88f5fcfd853908
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-08-18 11:22:23 -0300

    feat: Loading line

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { ApolloProvider } from '@apollo/client'
 import { client } from './../services/apollo'
 
+import NextNProgress from 'nextjs-progressbar'
+
 import { Layout } from './../components/layout/Layout'
 import 'tailwindcss/tailwind.css'
 import './../styles/globals.css'
@@ -10,6 +12,9 @@ import './../styles/custom-swiper.css'
 function MyApp({ Component, pageProps }) {
   return (
     <>
+    <NextNProgress
+      color='#5271F2'
+    />
     <ApolloProvider client={ client }>
       <Layout>
         <Component { ...pageProps } />
