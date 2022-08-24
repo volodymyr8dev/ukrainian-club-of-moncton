62a12ee510652fcbb8ccd00b3b3973f7162372bb
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-08-24 13:57:32 -0300

    build: Disable init function by passing no args

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -7,11 +7,11 @@ import * as Sentry from '@sentry/nextjs';
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://1e51f9732f394085a4d7453b15a4f85d@o1374104.ingest.sentry.io/6681251',
+  // dsn: SENTRY_DSN || 'https://1e51f9732f394085a4d7453b15a4f85d@o1374104.ingest.sentry.io/6681251',
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1.0,
-  debug: process.env.NODE_ENV !== 'production',
-  enabled: true,
+  // tracesSampleRate: 1.0,
+  // debug: process.env.NODE_ENV !== 'production',
+  // enabled: true,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
