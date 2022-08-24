ed8e47f46b6162f9d4ea69f40123402b0a920e87
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-08-24 13:50:12 -0300

    feat: Implement revalidate api handler

---
+++ pages/api/revalidate.js
@@ -0,0 +1,18 @@
+import path from 'path'
+
+export default async function handler(request, response) {
+  if (request.query.secret !== process.env.REVALIDATE_TOKEN) {
+    return response.status(401).json({ message: 'Invalid token' })
+  }
+
+  if (!request.body) {
+    return response.status(422).json({ message: 'Invalid requestuest body' })
+  }
+
+  try {
+    await response.revalidate(path.join('/', request.body.data.slug))
+    return response.status(200).json({ revalidated: true })
+  } catch (err) {
+    return response.status(500).send('Error revalidating')
+  }
+}
